62a1e2b2ce3df996ebdf1238fe11ac60907d386a
Robert Byrne <bobbyrne01@gmail.com>
2012-12-13 01:17:07 +0000

    Piece movement - Setting all images to be draggable

--- js/CreateBoard.js
+++ js/CreateBoard.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
 	var piece = new BasePiece(1, 2, false) ;
 	
 	var pawn = new Pawn(3, 4, 5, true);
+	//var board = new Board();
+});
+
+// Set all images to draggable
+$(function() {
+    $('img').draggable();
 });
