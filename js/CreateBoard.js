29e01c398fc559b0d72d8340d8c3cf53f10c16f9
Robert Byrne <bobbyrne01@gmail.com>
2012-12-22 15:16:51 +0000

    Piece selection - Changing event handler to 'mousedown', more appropriate as users will drag game pieces

--- js/CreateBoard.js
+++ js/CreateBoard.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
 	var piece = new BasePiece(1, 2, false) ;
 	
 	var pawn = new Pawn(3, 4, 5, true);
-	var board = new Board();
+	//var board = new Board();
 	
 	// Event handler for clicking squares on board
-	$('body').on('click', '#board td', function(e) {
+	$('body').on('mousedown', '#board td', function(e) {
 
-		alert($(this).children().attr('id'));
+		$('#debug').text("Clicked: " + $(this).children().attr('id'));
 	});
 });
 
